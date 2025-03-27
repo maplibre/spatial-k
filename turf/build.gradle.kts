de66b0124e23f9fb27fb59f7408b85a5bb41d97e
Dayan Ruben <mail@dayanruben.com>
2025-03-27 11:27:54 -0300

    Add benchmarking support and restructure source sets
    Introduced Kotlinx Benchmark plugin and added configurations for `jvmBench`, `jsBench`, and `nativeBench` targets. Updated source set dependencies and refactored to streamline platform-specific configurations. Improved language settings for better experimental feature utilization.

--- turf/build.gradle.kts
+++ turf/build.gradle.kts
@@ -1,149 +1,167 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.dokka)
     alias(libs.plugins.publish)
+    alias(libs.plugins.kotlinx.benchmark)
     alias(libs.plugins.resources)
 }
 
 kotlin {
     explicitApi()
 
-    jvm()
-    // Disable JavaScript target to work around the npm directory issue
-    // js {
-    //     browser()
-    //     nodejs()
-    //     binaries.executable()
-    // }
+    jvm {
+        compilations.create("bench")
+    }
+    js {
+        browser {
+        }
+        nodejs {
+        }
+
+        compilations.create("bench")
+    }
     // For ARM, should be changed to iosArm32 or iosArm64
     // For Linux, should be changed to e.g. linuxX64
     // For MacOS, should be changed to e.g. macosX64
     // For Windows, should be changed to e.g. mingwX64
-
-    linuxX64("native")
+    linuxX64("native") {
+        compilations.create("bench")
+    }
     mingwX64("mingw")
     macosX64("macos")
     iosArm64()
     iosX64()
     iosSimulatorArm64()
 
-    sourceSets["commonMain"].dependencies {
-        api(project(":geojson"))
-    }
-
-    sourceSets["commonTest"].dependencies {
-        implementation(kotlin("test"))
-        implementation(kotlin("test-annotations-common"))
-        implementation(libs.resources)
-    }
-
-    sourceSets["jvmMain"].dependencies {
-    }
-
-    sourceSets["jvmTest"].dependencies {
-    }
-
-    // JavaScript target is disabled
-    // sourceSets["jsMain"].dependencies {
-    // }
-
-    // sourceSets["jsTest"].dependencies {
-    // }
-
     sourceSets {
-        val commonMain by getting {}
-        val commonTest by getting {}
+        all {
+            with(languageSettings) {
+                optIn("kotlin.RequiresOptIn")
+                optIn("kotlin.js.ExperimentalJsExport")
+                optIn("kotlinx.serialization.InternalSerializationApi")
+                optIn("kotlinx.serialization.ExperimentalSerializationApi")
+            }
+        }
 
-        // JVM intermediate source sets
+        val commonMain by getting {
+            dependencies {
+                api(project(":geojson"))
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.resources)
+                implementation(kotlin("test"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jsMain by getting {}
+        val jsCommonMain by creating {
+            dependsOn(commonMain)
+        }
+        jsMain.dependsOn(jsCommonMain)
+
+        val jvmMain by getting {}
         val jvmCommonMain by creating {
             dependsOn(commonMain)
         }
-        val jvmCommonTest by creating {
-            dependsOn(commonTest)
-        }
-        val jvmMain by getting {
-            dependsOn(jvmCommonMain)
-        }
-        val jvmTest by getting {
-            dependsOn(jvmCommonTest)
-        }
+        jvmMain.dependsOn(jvmCommonMain)
 
-        // JS intermediate source sets are disabled
-        // val jsCommonMain by creating {
-        //     dependsOn(commonMain)
-        // }
-        // val jsCommonTest by creating {
-        //     dependsOn(commonTest)
-        // }
-        // val jsMain by getting {
-        //     dependsOn(jsCommonMain)
-        // }
-        // val jsTest by getting {
-        //     dependsOn(jsCommonTest)
-        // }
-
-        val nativeMain by getting {
+        val nativeMain by getting {}
+        val nativeCommonMain by creating {
             dependsOn(commonMain)
         }
-        getByName("macosMain").dependsOn(nativeMain)
-        getByName("mingwMain").dependsOn(nativeMain)
+        nativeMain.dependsOn(nativeCommonMain)
+
+        val macosMain by getting
+        val mingwMain by getting
+        macosMain.dependsOn(nativeCommonMain)
+        mingwMain.dependsOn(nativeCommonMain)
+
+        val nativeTest by getting {}
+        val nativeCommonTest by creating {
+            dependsOn(commonTest)
+        }
+        nativeTest.dependsOn(nativeCommonTest)
+
+        val macosTest by getting
+        val mingwTest by getting
+        macosTest.dependsOn(nativeCommonTest)
+        mingwTest.dependsOn(nativeCommonTest)
+
+        val commonBench by creating {
+            dependencies {
+                implementation(libs.kotlinx.benchmark)
+            }
+        }
+
+        val jsBenchCommon by creating {
+            dependsOn(commonBench)
+        }
+        val jsBench by getting {
+            dependsOn(jsBenchCommon)
+        }
+
+        val jvmBenchCommon by creating {
+            dependsOn(commonBench)
+        }
+        val jvmBench by getting {
+            dependsOn(jvmBenchCommon)
+        }
+
+        val nativeBenchCommon by creating {
+            dependsOn(commonBench)
+        }
+        val nativeBench by getting {
+            dependsOn(nativeBenchCommon)
+        }
+
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
-            dependsOn(nativeMain)
+            dependsOn(nativeCommonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
 
-        val nativeTest by getting {}
-        getByName("macosTest").dependsOn(nativeTest)
-        getByName("mingwTest").dependsOn(nativeTest)
         val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
-            dependsOn(nativeTest)
+            dependsOn(nativeCommonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
-
-        all {
-            with(languageSettings) {
-                optIn("kotlin.RequiresOptIn")
-            }
-        }
     }
 }
 
-// JavaScript target is disabled
-// tasks.named("jsBrowserTest") { enabled = false }
+benchmark {
+    this.configurations {
+        getByName("main") {
+            iterations = 5
+        }
+    }
 
-// Configure Dokka tasks
+    targets {
+        register("jvmBench")
+        register("jsBench")
+        register("nativeBench")
+    }
+}
+
 tasks.withType<org.jetbrains.dokka.gradle.AbstractDokkaTask>().configureEach {
-    // Disable Dokka tasks to work around the npm directory issue
-    enabled = false
     outputDirectory.set(layout.buildDirectory.dir("docs/api"))
 }
 
-tasks.register<Jar>("dokkaJavadocJar") {
-    archiveClassifier.set("javadoc")
-    from(tasks.named("dokkaGeneratePublicationHtml"))
-}
-
 tasks.withType(KotlinCompile::class.java).configureEach {
-    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
-}
-
-// Working around dokka problems
-afterEvaluate {
-    tasks.named("dokkaJavadocJar").configure {
-        dependsOn(":geojson:dokkaGeneratePublicationHtml")
-    }
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
 }
