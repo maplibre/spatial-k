6960d8d1d5e7ee87fc1ef61600c969f8d9101385
Jakob Gillich <jakob@gillich.me>
2025-08-30 06:11:25 +0200

    Add simplify

--- turf/src/commonTest/kotlin/io/github/dellisd/spatialk/turf/TransformationTest.kt
+++ turf/src/commonTest/kotlin/io/github/dellisd/spatialk/turf/TransformationTest.kt
@@ -2,7 +2,9 @@ package io.github.dellisd.spatialk.turf
 
 import io.github.dellisd.spatialk.geojson.Feature
 import io.github.dellisd.spatialk.geojson.LineString
+import io.github.dellisd.spatialk.geojson.Position
 import io.github.dellisd.spatialk.turf.utils.readResource
+import kotlin.math.roundToInt
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -36,5 +38,19 @@ class TransformationTest {
         val expectedOut = Feature.fromJson(readResource("transformation/bezierspline/out/issue-#1063.json"))
 
         assertEquals(expectedOut.geometry, bezierSpline(feature.geometry as LineString))
+    }
+
+    @Test
+    fun testSimplifyLineString() {
+        val feature = Feature.fromJson(readResource("transformation/simplify/in/linestring.json"))
+        val expected = Feature.fromJson(readResource("transformation/simplify/out/linestring.json"))
+        val simplified = simplify(feature.geometry as LineString, 0.01, false)
+        val roundedSimplified = LineString(simplified.coordinates.map { position ->
+            Position(
+                (position.longitude * 1000000).roundToInt() / 1000000.0,
+                (position.latitude * 1000000).roundToInt() / 1000000.0
+            )
+        })
+        assertEquals(expected.geometry as LineString, roundedSimplified)
     }
 }
