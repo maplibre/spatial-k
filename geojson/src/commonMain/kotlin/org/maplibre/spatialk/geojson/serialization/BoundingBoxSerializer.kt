a6fc1523a29166fc33d211e50362f58914e27885
Sargun Vohra <sargunv@users.noreply.github.com>
2025-09-30 17:57:41 -0700

    serializer

--- geojson/src/commonMain/kotlin/org/maplibre/spatialk/geojson/serialization/BoundingBoxSerializer.kt
+++ geojson/src/commonMain/kotlin/org/maplibre/spatialk/geojson/serialization/BoundingBoxSerializer.kt
@@ -12,8 +12,6 @@ import kotlinx.serialization.encoding.Encoder
 import org.maplibre.spatialk.geojson.BoundingBox
 
 public object BoundingBoxSerializer : KSerializer<BoundingBox> {
-    private const val ARRAY_SIZE_2D = 4
-    private const val ARRAY_SIZE_3D = 6
 
     @OptIn(ExperimentalSerializationApi::class)
     override val descriptor: SerialDescriptor = listSerialDescriptor(Double.serializer().descriptor)
@@ -21,9 +19,9 @@ public object BoundingBoxSerializer : KSerializer<BoundingBox> {
     override fun deserialize(decoder: Decoder): BoundingBox {
         val list = ListSerializer(Double.serializer()).deserialize(decoder)
 
-        if (list.size != ARRAY_SIZE_2D && list.size != ARRAY_SIZE_3D) {
+        if (list.size < 4 || list.size % 2 == 1) {
             throw SerializationException(
-                "Expected array of size 4 or 6. Got array of size ${list.size}"
+                "Expected array of even size >= 4. Got array of size ${list.size}"
             )
         }
 
