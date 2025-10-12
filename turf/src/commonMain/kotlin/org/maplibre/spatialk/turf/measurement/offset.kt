982ab5a9c375db8264d4474883b859c62f076c0e
Sargun Vohra <sargunv@users.noreply.github.com>
2025-10-12 15:42:56 -0700

    rename

--- turf/src/commonMain/kotlin/org/maplibre/spatialk/turf/measurement/offset.kt
+++ turf/src/commonMain/kotlin/org/maplibre/spatialk/turf/measurement/offset.kt
@@ -30,15 +30,15 @@ import org.maplibre.spatialk.units.extensions.*
 public fun Position.offset(distance: Length, bearing: Bearing): Position {
     val longitude1 = this.longitude.degrees
     val latitude1 = this.latitude.degrees
-    val bearingRad = (bearing - North)
+    val bearingFromN = (bearing - North)
     val radians = distance.inEarthRadians.radians
 
     val latitude2 =
-        asin(sin(latitude1) * cos(radians) + cos(latitude1) * sin(radians) * cos(bearingRad))
+        asin(sin(latitude1) * cos(radians) + cos(latitude1) * sin(radians) * cos(bearingFromN))
     val longitude2 =
         longitude1 +
             atan2(
-                sin(bearingRad) * sin(radians) * cos(latitude1),
+                sin(bearingFromN) * sin(radians) * cos(latitude1),
                 cos(radians) - sin(latitude1) * sin(latitude2),
             )
 
