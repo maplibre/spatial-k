d73fc22dec6d1e7e0672ae5e39ee7c5d398a45d1
Sargun Vohra <sargunv@users.noreply.github.com>
2025-11-09 12:22:41 -0800

    allow toJson with star projections (#284)

--- geojson/src/commonTest/kotlin/org/maplibre/spatialk/geojson/FeatureTest.kt
+++ geojson/src/commonTest/kotlin/org/maplibre/spatialk/geojson/FeatureTest.kt
@@ -508,10 +508,10 @@ class FeatureTest {
         // Map<*, *> properties not tested because the fallback serializer selection does not
         // support generic types.
 
-        val nullPropsFeature: GeoJsonObject = Feature(null, null)
-        val jsonObjectPropsFeature: GeoJsonObject =
+        val nullPropsFeature: Feature<*, *> = Feature(null, null)
+        val jsonObjectPropsFeature: Feature<*, *> =
             Feature(null, JsonObject(mapOf("key" to JsonPrimitive("value"))))
-        val dataClassPropsFeature: GeoJsonObject = Feature(null, NameProp("test"))
+        val dataClassPropsFeature: Feature<*, *> = Feature(null, NameProp("test"))
 
         val nullDeserialized = GeoJsonObject.fromJson(nullPropsFeature.toJson()) as Feature<*, *>
         val jsonObjectDeserialized =
