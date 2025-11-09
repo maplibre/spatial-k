da856d27185cb9b4cc6a4daab680b0221e40b8b8
Jakob Gillich <jakob@gillich.me>
2025-11-09 14:13:30 +0100

    reduce benchmark data size
    fix oom error on ci

--- benchmark/src/commonMain/kotlin/org/maplibre/spatialk/turf/RTreeBenchmark.kt
+++ benchmark/src/commonMain/kotlin/org/maplibre/spatialk/turf/RTreeBenchmark.kt
@@ -1,4 +1,4 @@
-package org.maplibre.spatialk.geojson
+package org.maplibre.spatialk.turf
 
 import kotlin.random.Random
 import kotlinx.benchmark.Benchmark
@@ -10,6 +10,12 @@ import kotlinx.benchmark.Scope
 import kotlinx.benchmark.Setup
 import kotlinx.benchmark.State
 import kotlinx.serialization.json.JsonObject
+import org.maplibre.spatialk.geojson.BoundingBox
+import org.maplibre.spatialk.geojson.Feature
+import org.maplibre.spatialk.geojson.FeatureCollection
+import org.maplibre.spatialk.geojson.Geometry
+import org.maplibre.spatialk.geojson.Point
+import org.maplibre.spatialk.geojson.Position
 import org.maplibre.spatialk.geojson.dsl.addFeature
 import org.maplibre.spatialk.geojson.dsl.buildFeatureCollection
 import org.maplibre.spatialk.turf.other.RTree
@@ -20,12 +26,14 @@ import org.maplibre.spatialk.turf.other.RTree
 open class RTreeBenchmark {
     private lateinit var featureCollection: FeatureCollection<Geometry?, JsonObject?>
     private lateinit var rtree16: RTree<Feature<Geometry?, JsonObject?>>
+    private lateinit var rtree32: RTree<Feature<Geometry?, JsonObject?>>
+    private lateinit var rtree64: RTree<Feature<Geometry?, JsonObject?>>
     private lateinit var rtree128: RTree<Feature<Geometry?, JsonObject?>>
 
     private val random = Random(0)
 
     private fun generateDataset() = buildFeatureCollection {
-        repeat(5000000) {
+        repeat(100000) {
             addFeature(
                 geometry =
                     Point(
@@ -41,35 +49,57 @@ open class RTreeBenchmark {
     @Setup
     fun setup() {
         featureCollection = generateDataset()
-        rtree16 = RTree(featureCollection.features, 32)
+        rtree16 = RTree(featureCollection.features, 16)
+        rtree32 = RTree(featureCollection.features, 32)
+        rtree64 = RTree(featureCollection.features, 64)
         rtree128 = RTree(featureCollection.features, 128)
     }
 
-    /** Benchmark insertion with entries=50 */
     @Benchmark
-    fun insertionFast() {
+    fun insertion128() {
         val rtree = RTree(featureCollection.features, 128)
         require(rtree.all().size == featureCollection.features.size)
     }
 
-    /** Benchmark insertion with entries=5 */
     @Benchmark
-    fun insertionSlow() {
+    fun insertion64() {
+        val rtree = RTree(featureCollection.features, 64)
+        require(rtree.all().size == featureCollection.features.size)
+    }
+
+    @Benchmark
+    fun insertion32() {
         val rtree = RTree(featureCollection.features, 32)
         require(rtree.all().size == featureCollection.features.size)
     }
 
-    /** Benchmark search with entries=5 */
     @Benchmark
-    fun searchFast() {
-        val result = rtree16.search(BoundingBox(Position(10.0, 10.0), Position(20.0, 20.0)))
-        require(result.size == 7665) { "Wrong number of results (${result.size})" }
+    fun insertion16() {
+        val rtree = RTree(featureCollection.features, 16)
+        require(rtree.all().size == featureCollection.features.size)
     }
 
-    /** Benchmark search with entries=50 */
     @Benchmark
-    fun searchSlow() {
+    fun search128() {
         val result = rtree128.search(BoundingBox(Position(10.0, 10.0), Position(20.0, 20.0)))
-        require(result.size == 7665) { "Wrong number of results (${result.size})" }
+        require(result.size == 149) { "Wrong number of results (${result.size})" }
+    }
+
+    @Benchmark
+    fun search64() {
+        val result = rtree64.search(BoundingBox(Position(10.0, 10.0), Position(20.0, 20.0)))
+        require(result.size == 149) { "Wrong number of results (${result.size})" }
+    }
+
+    @Benchmark
+    fun search32() {
+        val result = rtree32.search(BoundingBox(Position(10.0, 10.0), Position(20.0, 20.0)))
+        require(result.size == 149) { "Wrong number of results (${result.size})" }
+    }
+
+    @Benchmark
+    fun search16() {
+        val result = rtree16.search(BoundingBox(Position(10.0, 10.0), Position(20.0, 20.0)))
+        require(result.size == 149) { "Wrong number of results (${result.size})" }
     }
 }
